acdbfa4bbb4e2775edaaacf2736c08bafd594567
Jim Garrison <jim@garrison.cc>
2021-09-19 23:39:52 -0400

    Working build

--- builder.sh
+++ builder.sh
@@ -8,5 +8,10 @@ cp -r "$ekamSrc" ekam
 chmod -R u+w ekam
 cd ekam
 mkdir -p deps
-ln -s "$capnprotoSrc" deps/capnproto
+cp -r "$capnprotoSrc" deps/capnproto
+chmod u+w deps/capnproto/c++/src/kj -R
+sed -i 's/\/var\/tmp/\/tmp/g' deps/capnproto/c++/src/kj/filesystem-disk-test.c++
+unset NIX_ENFORCE_PURITY
 make
+mkdir $out
+cp -r bin $out
