f18fd808a3d4a797d0ab887931b13f39f023048d
Ian Denhardt <ian@zenhack.net>
2021-09-20 14:38:04 -0400

    Further clarify workarounds in builder.sh

--- builder.sh
+++ builder.sh
@@ -8,17 +8,21 @@ cp -r "$ekamSrc" ekam
 chmod -R u+w ekam
 cd ekam
 mkdir -p deps
+
 # A single capnproto test file expects to be able to write to
 # /var/tmp.  We change it to use /tmp because /var is not available
 # under nix-build.
 cp -r "$capnprotoSrc" deps/capnproto
 chmod u+w deps/capnproto/c++/src/kj -R
 sed -i 's/\/var\/tmp/\/tmp/g' deps/capnproto/c++/src/kj/filesystem-disk-test.c++
-# Turn off the purity check because it doesn't fare well with ekam.
-# This is also done in a handful of other expressions in nixpkgs.  See
-# discussion at https://github.com/NixOS/nixpkgs/issues/13532 and at
-# https://github.com/zenhack/ekam-nix/pull/4
+
+# NIX_ENFORCE_PURITY prevents ld from linking against anything outside of the
+# nix store -- but ekam builds capnp locally and links against it, so that causes
+# the build to fail. So, we turn this off.
+#
+# See: https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
 unset NIX_ENFORCE_PURITY
+
 make
 mkdir $out
 cp -r bin $out
